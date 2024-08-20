e185f464edb6bd912d8ad3253cf9c00fb12d2cfd
Ruddy <kael.ch2@gmail.com>
2024-08-20 10:38:59 -0400

    Prueba

---
+++ config/global.php
@@ -0,0 +1,8 @@
+<?php
+    define("SERVER","localhost");
+    define("USER","root");
+    define("PASSWORD","redes2024");
+    define("DATABASE","notas_master");
+    define("SET_NAMES","utf8");
+?>
+
